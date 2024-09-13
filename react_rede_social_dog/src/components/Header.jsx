495dda7ca919cdf4711dcbadda2437ec7650574e
frankMarcelino <frank.silva.ti@gmail.com>
2024-09-13 17:40:30 -0300

    feet: header feito

--- react_rede_social_dog/src/components/Header.jsx
+++ react_rede_social_dog/src/components/Header.jsx
@@ -1,13 +1,19 @@
 import React from "react";
-// import styles from "./Header.module.css";
+import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
+import Dogs from "../Assets/dogs.svg?react";
 
 const Header = () => {
   return (
-    <header>
-      <Link to="/">Home</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/cadastro">Cadastro</Link>
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} to="/" aria-label="Dogs - Home">
+          <Dogs />
+        </Link>
+        <Link className={styles.login} to="/login">
+          Login / Criar
+        </Link>
+      </nav>
     </header>
   );
 };
