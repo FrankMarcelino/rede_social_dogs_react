de91c241cc363f41d43fde650499131bb927511c
frankMarcelino <frank.silva.ti@gmail.com>
2024-09-14 16:01:06 -0300

    comecei usar Hooks

--- react_rede_social_dog/src/components/Forms/Input.jsx
+++ react_rede_social_dog/src/components/Forms/Input.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import styles from "./Input.module.css";
 
-const input = ({ label, type, name }) => {
+const input = ({ label, type, name, value, onChange }) => {
   return (
     <div className={styles.wrapper}>
       <label htmlFor={name} className={styles.label}>
         {label}
       </label>
-      <input id={name} className={styles.input} type={type} />
+      <input
+        id={name}
+        className={styles.input}
+        type={type}
+        name={name}
+        value={value}
+        onChange={onChange}
+      />
       <p className={styles.error}>Error</p>
     </div>
   );
--- react_rede_social_dog/src/components/Login/LoginForm.jsx
+++ react_rede_social_dog/src/components/Login/LoginForm.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Input from "../Forms/Input";
 import Button from "../Forms/Button";
+import useForm from "../../Hooks/useForm";
 
 function LoginForm() {
-  const [username, setUsername] = React.useState("");
-  const [password, setPassword] = React.useState("");
+  const username = useForm();
+  console.log(username);
+  const password = useForm();
+  console.log(password);
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -14,33 +17,26 @@ function LoginForm() {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ username, password }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(),
     })
-      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
       .then((json) => {
         console.log(json);
-        localStorage.setItem("token", json.token);
       });
   }
   return (
     <section>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <Input
-          label="Usuário"
-          name="username"
-          type="text"
-          value={username}
-          onChange={setUsername}
-        />
+        <Input label="Usuário" type="text" name="username" {...username} />
 
-        <Input
-          label="Senha"
-          name="password"
-          type="password"
-          value={password}
-          onChange={setPassword}
-        />
+        <Input label="Senha" type="password" name="password" {...password} />
 
         <Button>Entrar</Button>
       </form>
