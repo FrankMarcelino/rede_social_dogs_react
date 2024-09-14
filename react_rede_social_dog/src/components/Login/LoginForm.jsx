aa4b2bf3c5bad5d3d4e0eac3fec6d87423733fb6
frankMarcelino <frank.silva.ti@gmail.com>
2024-09-14 17:56:22 -0300

    feet: useForm quase pronto

--- react_rede_social_dog/src/components/Login/LoginForm.jsx
+++ react_rede_social_dog/src/components/Login/LoginForm.jsx
@@ -6,29 +6,30 @@ import useForm from "../../Hooks/useForm";
 
 function LoginForm() {
   const username = useForm();
-  console.log(username);
   const password = useForm();
-  console.log(password);
 
   function handleSubmit(event) {
     event.preventDefault();
-    fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(),
-    })
-      .then((response) => {
-        console.log(response);
-        return response.json();
+
+    if (username.validate() && password.validate()) {
+      fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(),
       })
-      .then((json) => {
-        console.log(json);
-      });
+        .then((response) => {
+          console.log(response);
+          return response.json();
+        })
+        .then((json) => {
+          console.log(json);
+        });
+    }
   }
   return (
     <section>
