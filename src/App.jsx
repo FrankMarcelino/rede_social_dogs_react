6dd60ce3b0eb8e65bbc47d5032f06181d401ff94
frankMarcelino <frank.silva.ti@gmail.com>
2024-09-29 22:29:06 -0300

    correção arquivo Home

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
-import Home from "./components/home.jsx";
+import Home from "./components/Home.jsx";
 import Login from "./components/Login/Login.jsx";
 import UserStorage from "./UserContext";
 import User from "./components/User/User.jsx";
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Feed from "./Feed/Feed";
 import Head from "./Helper/Head";
 
-const home = () => {
+const Home = () => {
   return (
     <section className="container mainContainer">
       <Head title="Fotos" description="Home do site rede social para PET" />
@@ -11,4 +11,4 @@ const home = () => {
   );
 };
 
-export default home;
+export default Home;
